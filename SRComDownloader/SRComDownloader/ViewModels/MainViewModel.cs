e77f166f116642c8f64d4d5e76955742b1dfb54b
Phillip Smith <FilupErwinSmith@gmail.com>
2020-01-02 15:55:36 +1100

    Updated

---
+++ SRComDownloader/SRComDownloader/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: SRComDownloader
+// Project: SRComDownloader
+// File Name: MainViewModel.cs
+// 
+// Current Data:
+// 2020-01-02 3:54 PM
+// 
+// Creation Date:
+// 2020-01-02 3:53 PM
+
+#endregion
+
+namespace SRComDownloader.ViewModels
+{
+  internal class MainViewModel
+  {
+  }
+}
